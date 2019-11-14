426c2bb93a54394ee98a871bcb6a023a6244ac65
Chazzers <chazz@henryhandsome.nl>
2019-11-14 23:05:50 +0100

    remove console.logs from data-cleaning-assignment

--- clean-data-assignment/src/js/index.js
+++ clean-data-assignment/src/js/index.js
@@ -3,18 +3,15 @@ csv("../data/enquete.csv")
 	.then(data => makeArray(data))
 	.then(data => filterUndefined(data))
 	.then(data => filterKleurNaam(data))
-	.then(data => console.log(addHashtag(data)));
+	.then(data => addHashtag(data));
 
 	function makeArray(items) {
-		console.log(items);
 		return items.map(item => item["Kleur haar (HEX code)"].toUpperCase());
 	}
 	function filterUndefined(items) {
-		console.log(items);
 		return items.filter(item => item !== '' && item !== "0");
 	}
 	function filterKleurNaam(items) {
-		console.log(items);
 		return items.filter(item => !item.includes("BLOND") && !item.includes("BRUIN"))
 	}
 	function addHashtag(items) {
